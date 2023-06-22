618c5738b276dc452083071c70b683c90aa54d6f
William Pederzoli <wpederzoli@gmail.com>
2023-06-22 11:01:50 +0200

    Two platforms creation

--- src/platforms/mod.rs
+++ src/platforms/mod.rs
@@ -4,6 +4,7 @@ use crate::{WINDOW_HEIGHT, WINDOW_WIDTH};
 
 const PLATFORM_WIDTH: f32 = 300.;
 const PLATFORM_HEIGHT: f32 = 600.;
+const PLATFORM_LAYER: f32 = 1.;
 
 pub struct PlatformsPlugin;
 
@@ -18,19 +19,32 @@ fn setup(mut commands: Commands) {
     let y_size = 5.;
     for i in 1..=x_size as u32 {
         for j in 1..=y_size as u32 {
-            commands.spawn(SpriteBundle {
-                sprite: Sprite {
-                    color: Color::ORANGE_RED,
-                    custom_size: Some(Vec2::new(PLATFORM_WIDTH / x_size, PLATFORM_HEIGHT / y_size)),
-                    ..default()
-                },
-                transform: Transform::from_xyz(
-                    (-WINDOW_WIDTH / 2.) + (PLATFORM_WIDTH / x_size) * i as f32,
-                    (WINDOW_HEIGHT / 2.) - (PLATFORM_HEIGHT / y_size) * j as f32,
-                    1.,
-                ),
-                ..default()
-            });
+            commands.spawn(create_platform(
+                (-WINDOW_WIDTH / 2.) + (PLATFORM_WIDTH / x_size) * i as f32,
+                (WINDOW_HEIGHT / 2.) - (PLATFORM_HEIGHT / y_size) * j as f32,
+                Vec2::new(PLATFORM_WIDTH / x_size, PLATFORM_HEIGHT / y_size),
+            ));
         }
+    }
+    for i in 1..=x_size as u32 {
+        for j in 1..=y_size as u32 {
+            commands.spawn(create_platform(
+                (WINDOW_WIDTH / 2.) - (PLATFORM_WIDTH / x_size) * i as f32,
+                (WINDOW_HEIGHT / 2.) - (PLATFORM_HEIGHT / y_size) * j as f32,
+                Vec2::new(PLATFORM_WIDTH / x_size, PLATFORM_HEIGHT / y_size),
+            ));
+        }
+    }
+}
+
+fn create_platform(x: f32, y: f32, size: Vec2) -> SpriteBundle {
+    SpriteBundle {
+        sprite: Sprite {
+            color: Color::ORANGE_RED,
+            custom_size: Some(size),
+            ..default()
+        },
+        transform: Transform::from_xyz(x, y, PLATFORM_LAYER),
+        ..default()
     }
 }
