c7efc4a1c460116ef3611038c5d79ac66eaf4bc9
William Pederzoli <wpederzoli@gmail.com>
2023-06-22 10:53:07 +0200

    First platform implementation

--- src/platforms/mod.rs
+++ src/platforms/mod.rs
@@ -2,8 +2,8 @@ use bevy::prelude::*;
 
 use crate::{WINDOW_HEIGHT, WINDOW_WIDTH};
 
-const PLATFORM_WIDTH: f32 = 400.;
-const PLATFORM_HEIGHT: f32 = 700.;
+const PLATFORM_WIDTH: f32 = 300.;
+const PLATFORM_HEIGHT: f32 = 600.;
 
 pub struct PlatformsPlugin;
 
@@ -17,7 +17,7 @@ fn setup(mut commands: Commands) {
     let x_size = 3.;
     let y_size = 5.;
     for i in 1..=x_size as u32 {
-        for j in 0..y_size as u32 {
+        for j in 1..=y_size as u32 {
             commands.spawn(SpriteBundle {
                 sprite: Sprite {
                     color: Color::ORANGE_RED,
@@ -25,8 +25,8 @@ fn setup(mut commands: Commands) {
                     ..default()
                 },
                 transform: Transform::from_xyz(
-                    (-WINDOW_WIDTH / 2.) + (PLATFORM_WIDTH / x_size * i as f32) + 200.,
-                    PLATFORM_HEIGHT / y_size * j as f32,
+                    (-WINDOW_WIDTH / 2.) + (PLATFORM_WIDTH / x_size) * i as f32,
+                    (WINDOW_HEIGHT / 2.) - (PLATFORM_HEIGHT / y_size) * j as f32,
                     1.,
                 ),
                 ..default()
