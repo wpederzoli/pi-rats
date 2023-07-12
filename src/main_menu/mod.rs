6f03c46b6e765768c65eb4a7663358f51da88af1
William Pederzoli <wpederzoli@gmail.com>
2023-07-12 12:00:32 +0200

    Start button, hover and gameplay

--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -49,7 +49,7 @@ fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
                 })
                 .with_children(|parent| {
                     parent.spawn(TextBundle::from_section(
-                        "Play",
+                        "Start",
                         TextStyle {
                             font: asset_server.load("fonts/FiraSans-Bold.ttf"),
                             font_size: 40.0,
@@ -67,25 +67,18 @@ fn button_system(
         (&Interaction, &mut BackgroundColor, &Children),
         (Changed<Interaction>, With<Button>),
     >,
-    mut text_query: Query<&mut Text>,
     mut next_state: ResMut<NextState<GameState>>,
 ) {
-    for (interaction, mut color, children) in &mut interaction_query {
-        let mut text = text_query.get_mut(children[0]).unwrap();
+    for (interaction, mut color, _) in &mut interaction_query {
         match *interaction {
             Interaction::Clicked => {
-                text.sections[0].value = "Press".to_string();
                 next_state.set(GameState::GamePlay);
                 *color = PRESSED_BUTTON.into();
             }
             Interaction::Hovered => {
-                text.sections[0].value = "Hover".to_string();
-                *color = HOVERED_BUTTON.into();
+                *color = BackgroundColor(HOVERED_BUTTON);
             }
-            Interaction::None => {
-                text.sections[0].value = "Button".to_string();
-                *color = NORMAL_BUTTON.into();
-            }
+            _ => *color = BackgroundColor(NORMAL_BUTTON),
         }
     }
 }
