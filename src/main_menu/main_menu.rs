9350900f350f83c7e9672592f64b9057c689fc73
William Pederzoli <wpederzoli@gmail.com>
2023-07-13 11:36:46 +0200

    Start button with implementation. Main menu UI. Refactor

---
+++ src/main_menu/main_menu.rs
@@ -0,0 +1,54 @@
+use bevy::prelude::*;
+
+pub const MENU_SIZE: f32 = 400.;
+const BACKGROUND_COLOR: Color = Color::BLACK;
+
+#[derive(Component)]
+pub struct MenuButton {
+    pub button_type: MenuButtonType,
+}
+
+pub enum MenuButtonType {
+    StartButton,
+    CreateParty,
+}
+
+pub fn create_container(position: &UiRect) -> NodeBundle {
+    NodeBundle {
+        background_color: BackgroundColor(BACKGROUND_COLOR),
+        style: Style {
+            position: *position,
+            size: Size::new(Val::Px(MENU_SIZE), Val::Px(MENU_SIZE)),
+            align_items: AlignItems::Center,
+            padding: UiRect::new(Val::Px(0.), Val::Px(0.), Val::Px(35.), Val::Px(0.)),
+            flex_direction: FlexDirection::Column,
+            ..default()
+        },
+        ..default()
+    }
+}
+
+pub fn create_button(color: Color) -> ButtonBundle {
+    ButtonBundle {
+        style: Style {
+            size: Size::new(Val::Px(200.0), Val::Px(65.0)),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            margin: UiRect::new(Val::Px(0.), Val::Px(0.), Val::Px(0.), Val::Px(15.)),
+            ..default()
+        },
+        background_color: color.into(),
+        ..default()
+    }
+}
+
+pub fn create_text(text: &str, font: Handle<Font>) -> TextBundle {
+    TextBundle::from_section(
+        text,
+        TextStyle {
+            font,
+            font_size: 40.0,
+            color: Color::rgb(0.9, 0.9, 0.9),
+        },
+    )
+}
--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -1,13 +1,17 @@
 use bevy::prelude::*;
 
-use crate::GameState;
+mod main_menu;
+
+use crate::{GameState, WINDOW_HEIGHT, WINDOW_WIDTH};
+
+use self::main_menu::{
+    create_button, create_container, create_text, MenuButton, MenuButtonType, MENU_SIZE,
+};
 
 pub struct MainMenuPlugin;
 
-#[derive(Resource)]
-struct MenuData {
-    button_entity: Entity,
-}
+#[derive(Component)]
+struct MainMenu;
 
 const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
@@ -22,57 +26,58 @@ impl Plugin for MainMenuPlugin {
 }
 
 fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let button_entity = commands
-        .spawn(NodeBundle {
-            style: Style {
-                // center button
-                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                ..default()
-            },
-            ..default()
+    let menu_position = UiRect::new(
+        Val::Px((WINDOW_WIDTH / 2.) - MENU_SIZE / 2.),
+        Val::Px(0.),
+        Val::Px((WINDOW_HEIGHT / 2.) - MENU_SIZE / 2.),
+        Val::Px(0.),
+    );
+
+    commands
+        .spawn(create_container(&menu_position))
+        .with_children(|parent| {
+            parent
+                .spawn(create_button(NORMAL_BUTTON))
+                .with_children(|parent| {
+                    parent.spawn(create_text(
+                        "Start",
+                        asset_server.load("fonts/FiraSans-Bold.ttf"),
+                    ));
+                })
+                .insert(MenuButton {
+                    button_type: MenuButtonType::StartButton,
+                });
         })
         .with_children(|parent| {
             parent
-                .spawn(ButtonBundle {
-                    style: Style {
-                        size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    background_color: NORMAL_BUTTON.into(),
-                    ..default()
-                })
+                .spawn(create_button(NORMAL_BUTTON))
                 .with_children(|parent| {
-                    parent.spawn(TextBundle::from_section(
-                        "Start",
-                        TextStyle {
-                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                            font_size: 40.0,
-                            color: Color::rgb(0.9, 0.9, 0.9),
-                        },
+                    parent.spawn(create_text(
+                        "Create Party",
+                        asset_server.load("fonts/FiraSans-Bold.ttf"),
                     ));
+                })
+                .insert(MenuButton {
+                    button_type: MenuButtonType::CreateParty,
                 });
         })
-        .id();
-    commands.insert_resource(MenuData { button_entity });
+        .insert(MainMenu);
 }
 
 fn button_system(
     mut interaction_query: Query<
-        (&Interaction, &mut BackgroundColor, &Children),
+        (&Interaction, &mut BackgroundColor, &MenuButton),
         (Changed<Interaction>, With<Button>),
     >,
     mut next_state: ResMut<NextState<GameState>>,
 ) {
-    for (interaction, mut color, _) in &mut interaction_query {
+    for (interaction, mut color, button) in &mut interaction_query {
         match *interaction {
             Interaction::Clicked => {
-                next_state.set(GameState::GamePlay);
+                match button.button_type {
+                    MenuButtonType::StartButton => next_state.set(GameState::GamePlay),
+                    _ => (),
+                }
                 *color = PRESSED_BUTTON.into();
             }
             Interaction::Hovered => {
@@ -83,6 +88,7 @@ fn button_system(
     }
 }
 
-fn cleanup_menu(mut commands: Commands, menu_data: Res<MenuData>) {
-    commands.entity(menu_data.button_entity).despawn_recursive();
+fn cleanup_menu(mut commands: Commands, main_menu: Query<Entity, With<MainMenu>>) {
+    let main_menu = main_menu.single();
+    commands.entity(main_menu).despawn_recursive();
 }
