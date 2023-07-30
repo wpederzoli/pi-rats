25bc52699c483f00d52c1a3d06ef244e3fc344f2
William Pederzoli <wpederzoli@gmail.com>
2023-07-30 13:36:15 +0200

    Create and join room

--- client/src/network/mod.rs
+++ client/src/network/mod.rs
@@ -1,65 +1,55 @@
 use awc::{ws, Client};
 use bevy::prelude::*;
 use futures_util::{SinkExt as _, StreamExt as _};
-use std::{
-    sync::{
-        mpsc::{channel, Receiver, Sender},
-        Mutex,
-    },
-    thread,
-};
+
+use crate::GameState;
 
 pub struct NetworkPlugin;
 pub struct CreatePartyEvent;
-
-#[derive(Resource)]
-pub struct Channel(Option<Mutex<Sender<ws::Message>>>);
+pub struct JoinRoomEvent;
 
 impl Plugin for NetworkPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<CreatePartyEvent>()
-            .insert_resource(Channel(None))
+            .add_event::<JoinRoomEvent>()
             .add_system(create_party)
-            .add_system(test_ws);
+            .add_system(join_room);
     }
 }
 
-fn create_party(mut create_event: EventReader<CreatePartyEvent>, mut ch: ResMut<Channel>) {
-    for _ in create_event.iter() {
-        let (tx, rx) = channel::<ws::Message>();
-        ch.0 = Some(Mutex::new(tx));
-        thread::spawn(move || setup_network(rx));
-    }
-}
-
-fn test_ws(ch: ResMut<Channel>) {
-    if let Some(ch) = &ch.0 {
-        if let Ok(send) = ch.lock() {
-            let _ = send.send(ws::Message::Text("test".into()));
-        }
+fn join_room(
+    mut join_ev: EventReader<JoinRoomEvent>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
+    for _ in join_ev.iter() {
+        println!("join room");
+        next_state.set(GameState::Waiting);
     }
 }
 
 #[actix_rt::main]
-async fn setup_network(rx: Receiver<ws::Message>) {
-    let a = actix_rt::spawn(async move {
+async fn create_party(
+    mut create_ev: EventReader<CreatePartyEvent>,
+    mut join_ev: EventWriter<JoinRoomEvent>,
+) {
+    for _ in create_ev.iter() {
         let (_res, mut ws) = Client::new()
             .ws("ws://127.0.0.1:8080/ws/")
             .connect()
             .await
             .unwrap();
 
-        loop {
-            if let Ok(r) = rx.recv() {
-                println!("received: {:?}", r);
-                let _ = ws.send(r).await.unwrap();
-            }
+        ws.send(ws::Message::Text("create party".into()))
+            .await
+            .unwrap();
 
-            if let Ok(msg) = ws.next().await.unwrap() {
-                println!("message ws: {:?}", msg);
+        loop {
+            if let Ok(ws_msg) = ws.next().await.unwrap() {
+                //if message OK ROOM CREATED
+                println!("ws: {:?}", ws_msg);
+                join_ev.send(JoinRoomEvent);
+                break;
             }
         }
-    });
-
-    let _ = a.await;
+    }
 }
