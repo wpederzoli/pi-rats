4224c7763034557daac51f74d29a59cf1905c196
William Pederzoli <wpederzoli@gmail.com>
2023-08-04 14:29:25 +0200

    UI refactor

--- client/src/main_menu/mod.rs
+++ client/src/main_menu/mod.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 
 mod main_menu;
 mod modal;
+mod ui;
 
 use crate::{
     network::{CreatePartyEvent, JoinRoomEvent},
@@ -9,21 +10,15 @@ use crate::{
 };
 
 use self::{
-    main_menu::{
-        create_button, create_container, create_text, MenuButton, MenuButtonType, BACKGROUND_COLOR,
-        MENU_SIZE,
-    },
-    modal::spawn_modal,
+    main_menu::{MenuButton, MenuButtonType},
+    modal::{create_input_field, spawn_modal},
+    ui::{create_main_menu, HOVERED_BUTTON_COLOR, NORMAL_BUTTON_COLOR, PRESSED_BUTTON_COLOR},
 };
 
 pub struct MainMenuPlugin;
 
 #[derive(Component)]
 struct MainMenu;
-
-const NORMAL_BUTTON_COLOR: Color = Color::rgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON_COLOR: Color = Color::rgb(0.25, 0.25, 0.25);
-const PRESSED_BUTTON_COLOR: Color = Color::rgb(0.35, 0.75, 0.35);
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
@@ -33,59 +28,12 @@ impl Plugin for MainMenuPlugin {
     }
 }
 
-//TODO: refactor
 fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let menu_position = UiRect::new(
-        Val::Px((WINDOW_WIDTH / 2.) - MENU_SIZE / 2.),
-        Val::Px(0.),
-        Val::Px((WINDOW_HEIGHT / 2.) - MENU_SIZE / 2.),
-        Val::Px(0.),
-    );
-
-    commands
-        .spawn(create_container(&menu_position, &BACKGROUND_COLOR))
-        .with_children(|parent| {
-            parent
-                .spawn(create_button(NORMAL_BUTTON_COLOR))
-                .with_children(|parent| {
-                    parent.spawn(create_text(
-                        "Start",
-                        asset_server.load("fonts/FiraSans-Bold.ttf"),
-                    ));
-                })
-                .insert(MenuButton {
-                    button_type: MenuButtonType::StartButton,
-                });
-        })
-        .with_children(|parent| {
-            parent
-                .spawn(create_button(NORMAL_BUTTON_COLOR))
-                .with_children(|parent| {
-                    parent.spawn(create_text(
-                        "Create Party",
-                        asset_server.load("fonts/FiraSans-Bold.ttf"),
-                    ));
-                })
-                .insert(MenuButton {
-                    button_type: MenuButtonType::CreateParty,
-                });
-        })
-        .with_children(|parent| {
-            parent
-                .spawn(create_button(NORMAL_BUTTON_COLOR))
-                .with_children(|parent| {
-                    parent.spawn(create_text(
-                        "Join Party",
-                        asset_server.load("fonts/FiraSans-Bold.ttf"),
-                    ));
-                })
-                .insert(MenuButton {
-                    button_type: MenuButtonType::JoinParty,
-                });
-        })
-        .insert(MainMenu);
+    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
+    create_main_menu(&mut commands, font);
 }
 
+//TODO: Move logic to main menu
 fn button_system(
     mut interaction_query: Query<
         (&Interaction, &mut BackgroundColor, &MenuButton),
@@ -94,6 +42,7 @@ fn button_system(
     mut next_state: ResMut<NextState<GameState>>,
     mut commands: Commands,
     mut join_event: EventWriter<JoinRoomEvent>,
+    asset_server: Res<AssetServer>,
     menu: Query<Entity, With<MainMenu>>,
 ) {
     for menu in menu.iter() {
@@ -103,14 +52,9 @@ fn button_system(
                     match button.button_type {
                         MenuButtonType::StartButton => next_state.set(GameState::GamePlay),
                         MenuButtonType::CreateParty => {
-                            let modal = spawn_modal(&UiRect::new(
-                                Val::Px(0.),
-                                Val::Px(0.),
-                                Val::Px(-200.),
-                                Val::Px(0.),
-                            ));
-                            let modal_entity = commands.spawn(modal).id();
-                            commands.entity(menu).push_children(&[modal_entity]);
+                            let font: Handle<Font> = asset_server.load("fonts/FiraSans-Bold.ttf");
+                            let input_field = create_input_field(&mut commands, font);
+                            commands.entity(menu).push_children(&[input_field]);
                         }
                         MenuButtonType::JoinParty => {
                             join_event.send(JoinRoomEvent("test room".to_string()))
--- client/src/main_menu/modal.rs
+++ client/src/main_menu/modal.rs
@@ -1,8 +1,26 @@
 use bevy::{
-    prelude::{Color, NodeBundle},
+    prelude::{BuildChildren, Color, Commands, Entity, Handle, NodeBundle},
+    text::Font,
     ui::{BackgroundColor, Size, Style, UiRect, Val},
     utils::default,
 };
+
+use super::ui::{create_container, create_text};
+
+pub fn create_input_field(commands: &mut Commands, font: Handle<Font>) -> Entity {
+    let container = create_container(&UiRect::default(), &Color::DARK_GRAY);
+    let text = create_text("New party name: ", font);
+    let input_f = create_container(&UiRect::default(), &Color::ANTIQUE_WHITE);
+    commands
+        .spawn(container)
+        .with_children(|parent| {
+            parent.spawn(text);
+        })
+        .with_children(|parent| {
+            parent.spawn(input_f);
+        })
+        .id()
+}
 
 pub fn spawn_modal(position: &UiRect) -> NodeBundle {
     NodeBundle {
