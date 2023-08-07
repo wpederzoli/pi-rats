aef642aa3cf4eedf5d3ad8bfe1241dded94b8151
William Pederzoli <wpederzoli@gmail.com>
2023-08-07 19:40:59 +0200

    refactor create party function

--- client/src/network/mod.rs
+++ client/src/network/mod.rs
@@ -1,11 +1,11 @@
-use std::str::from_utf8;
-
-use awc::{ws, Client};
 use bevy::prelude::*;
 use common::RoomAction;
-use futures_util::{SinkExt as _, StreamExt as _};
 
 use crate::GameState;
+
+use self::room::connect_to_server;
+
+mod room;
 
 pub struct NetworkPlugin;
 pub struct CreatePartyEvent(pub String);
@@ -17,36 +17,6 @@ impl Plugin for NetworkPlugin {
             .add_event::<JoinRoomEvent>()
             .add_system(create_party);
     }
-}
-
-pub async fn connect_to_server(room_action: RoomAction) -> RoomAction {
-    let (_res, mut ws) = Client::new()
-        .ws("ws://127.0.0.1:8080/ws/")
-        .connect()
-        .await
-        .unwrap();
-
-    let json_msg = serde_json::to_string(&room_action).unwrap();
-    ws.send(ws::Message::Text(json_msg.into())).await.unwrap();
-
-    let mut response = RoomAction::Invalid("".to_string());
-
-    loop {
-        if let Ok(ws_msg) = ws.next().await.unwrap() {
-            match ws_msg {
-                ws::Frame::Text(msg) => match from_utf8(&msg) {
-                    Ok(txt) => {
-                        response = serde_json::from_str(txt).unwrap();
-                    }
-                    _ => response = RoomAction::Invalid("Unable to create room".to_string()),
-                },
-                _ => response = RoomAction::Invalid("Something went wrong".to_string()),
-            }
-        }
-        break;
-    }
-
-    response
 }
 
 #[actix_rt::main]
--- client/src/network/room.rs
+++ client/src/network/room.rs
@@ -1,11 +1,10 @@
 use std::str::from_utf8;
 
 use awc::{ws, Client};
-use bevy::prelude::info;
 use common::RoomAction;
 use futures_util::{SinkExt as _, StreamExt as _};
 
-pub async fn communicate_with_server(room_action: RoomAction) -> RoomAction {
+pub async fn connect_to_server(room_action: RoomAction) -> RoomAction {
     let (_res, mut ws) = Client::new()
         .ws("ws://127.0.0.1:8080/ws/")
         .connect()
@@ -15,15 +14,22 @@ pub async fn communicate_with_server(room_action: RoomAction) -> RoomAction {
     let json_msg = serde_json::to_string(&room_action).unwrap();
     ws.send(ws::Message::Text(json_msg.into())).await.unwrap();
 
+    let mut response = RoomAction::Invalid("".to_string());
+
     loop {
         if let Ok(ws_msg) = ws.next().await.unwrap() {
             match ws_msg {
                 ws::Frame::Text(msg) => match from_utf8(&msg) {
-                    Ok(txt) => serde_json::from_str(txt.clone()).unwrap(),
-                    _ => info!("Unexpected message: {:?}", msg),
+                    Ok(txt) => {
+                        response = serde_json::from_str(txt).unwrap();
+                    }
+                    _ => response = RoomAction::Invalid("Unable to create room".to_string()),
                 },
-                _ => info!("Failed to process response"),
+                _ => response = RoomAction::Invalid("Something went wrong".to_string()),
             }
         }
+        break;
     }
+
+    response
 }
